13ece0e94b5c713d260f451b195aa08be765be6b
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2014-01-06 15:52:35 +0900

    spdycat: Use SSLv23_client_method instead of TLSv1_client_method

--- src/spdycat.cc
+++ src/spdycat.cc
@@ -710,7 +710,7 @@ int communicate(const std::string& host, uint16_t port,
   }
 
   if(!config.no_tls) {
-    ssl_ctx = SSL_CTX_new(TLSv1_client_method());
+    ssl_ctx = SSL_CTX_new(SSLv23_client_method());
     if(!ssl_ctx) {
       std::cerr << ERR_error_string(ERR_get_error(), 0) << std::endl;
       result = -1;
